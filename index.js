9e88770402e3109e36eb6873b60f3ea57ae9fddf
Bejoy Chate <bejoy.chate@valpo.edu>
2023-12-08 04:58:49 -0600

    new message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 PORT = process.env.PORT || 8081
 app.get('/', function (req, res) {
   res.status(200)
-  .send('Hello class! This is a dummy project created as a part of Cloud Computing by Bejoy Chate & Vikramaditya Nagilla.')
+  .send('This is a dummy project created as a part of Cloud Computing by Bejoy Chate & Vikramaditya Nagilla.')
 })
 
 app.listen(PORT, () => console.log (`Server is running: http://localhost:${PORT}`))
